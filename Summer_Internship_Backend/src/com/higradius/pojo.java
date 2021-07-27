a2b62d8f20557ed6b2b7d71915ab70dfaca9fef0
Aadesh Parija <60508886+aadesh1214@users.noreply.github.com>
2021-07-27 07:53:22 +0530

    Add files via upload

---
+++ Summer_Internship_Backend/src/com/higradius/pojo.java
@@ -0,0 +1,56 @@
+package com.higradius;
+
+public class pojo {
+     
+	private String name_customer;
+	private String cust_number;
+	private String invoice_id;
+	private String total_open_amount;
+	private String due_in_date_1;
+	private String payment_predicted;
+	private String notes;
+	public String getName_customer() {
+		return name_customer;
+	}
+	public void setName_customer(String name_customer) {
+		this.name_customer = name_customer;
+	}
+	public String getCust_number() {
+		return cust_number;
+	}
+	public void setCust_number(String cust_number) {
+		this.cust_number = cust_number;
+	}
+	public String getInvoice_id() {
+		return invoice_id;
+	}
+	public void setInvoice_id(String invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	public String getTotal_open_amount() {
+		return total_open_amount;
+	}
+	public void setTotal_open_amount(String total_open_amount) {
+		this.total_open_amount = total_open_amount;
+	}
+	public String getDue_in_date_1() {
+		return due_in_date_1;
+	}
+	public void setDue_in_date_1(String due_in_date_1) {
+		this.due_in_date_1 = due_in_date_1;
+	}
+	public String getPayment_predicted() {
+		return payment_predicted;
+	}
+	public void setPayment_predicted(String payment_predicted) {
+		this.payment_predicted = payment_predicted;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+	
+}
